75ef3c0113b6e1ac4b796cc0a8839722f1219255
Kunalsonar007 <kunalwankhede958@gmail.com>
2024-10-31 16:25:35 +0530

    New Commit

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var config = {
   lines: [
-    "Dear Pookie 🫶🏼🐑",
+    "Dear Pookie 🐑",
     "Flowers bloom for a short time, but their memories stay with us forever.",
     "Our love is just like that – always fresh and special!",
     "Your smile fills my heart with happiness because you are my world.",
     "My love is only for you, and it will always be.",
     "Dear, these 44 months together have been so beautiful",
-    "I will love you forever 💓🍭",
+    "I will love you forever 💓",
   ],
   date: "2021-03-01T00:00:00",
   names: ["Pookie ", "Pig"],
